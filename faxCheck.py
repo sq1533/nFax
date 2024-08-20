814823247d0b09d20d35dab30d7ad4c93a21d5bd
sq1533 <618356@naver.com>
2024-08-20 17:02:39 +0900

    feat: 데이터 재정리

--- faxCheck.py
+++ faxCheck.py
@@ -47,7 +47,7 @@ def faxCheck():
                 faxNumber = fax_soup.find('div', attrs={'class':'t_row stt_notread faxReceiveBoxListRow'}).get('data-send-fax-number')
             else:
                 faxNumber = "확인불가"
-            if faxNumber in fax.loc['faxNumber'].tolist():
+            if faxNumber in fax['faxNumber'].tolist():
                 tell = f"신규 팩스 수신, 확인필요\n팩스번호 : {faxNumber}\n원천사 : {fax[fax['faxNumber'] == faxNumber]['원천사'].values}"
                 #텔레그램 전송
                 requests.get(f"https://api.telegram.org/bot{bot_info['token']}/sendMessage?chat_id={bot_info['chatId']}&text={tell}")
