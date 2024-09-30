dbd32ddf7dc81032423d171e1762c619eae9f56f
sq1533 <618356@naver.com>
2024-09-30 17:00:23 +0900

    feat : timeout 오류 재실행 코드 작성

--- faxCheck.py
+++ faxCheck.py
@@ -5,6 +5,7 @@ import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
 from bs4 import BeautifulSoup
 options = webdriver.ChromeOptions()
 options.add_argument("--headless")
@@ -40,33 +41,48 @@ class nfax:
         t.sleep(1)
     #엔팩스 메일 확인
     def newMail(self):
-        driver.get("https://mail.worksmobile.com/#/my/103")
-        t.sleep(2)
-        mailHome_soup = BeautifulSoup(driver.page_source,'html.parser')
-        if mailHome_soup.find('li', attrs={'class':'notRead'}) != None:
-            faxNumber = mailHome_soup.find('strong', attrs={'class':'mail_title'}).getText().replace(' ','').split("hecto_2f에")[1].split("로부터")[0]
-            if faxNumber in self.fax['faxNumber'].tolist():
-                tell = f"신규 팩스 수신, 확인필요\n팩스번호 : {faxNumber}\n원천사 : {self.fax[self.fax['faxNumber'] == faxNumber]['원천사'].values}"
-                requests.get(f"https://api.telegram.org/bot{self.bot_info['token']}/sendMessage?chat_id={self.bot_info['chatId']}&text={tell}")
+        try:
+            driver.get("https://mail.worksmobile.com/#/my/103")
+            t.sleep(2)
+            mailHome_soup = BeautifulSoup(driver.page_source,'html.parser')
+            if mailHome_soup.find('li', attrs={'class':'notRead'}) != None:
+                faxNumber = mailHome_soup.find('strong', attrs={'class':'mail_title'}).getText().replace(' ','').split("hecto_2f에")[1].split("로부터")[0]
+                if faxNumber in self.fax['faxNumber'].tolist():
+                    tell = f"신규 팩스 수신, 확인필요\n팩스번호 : {faxNumber}\n원천사 : {self.fax[self.fax['faxNumber'] == faxNumber]['원천사'].values}"
+                    requests.get(f"https://api.telegram.org/bot{self.bot_info['token']}/sendMessage?chat_id={self.bot_info['chatId']}&text={tell}")
+                else:
+                    tell = f"신규 팩스 수신, 확인필요\n팩스번호 : {faxNumber}\n원천사 : 확인불가"
+                    requests.get(f"https://api.telegram.org/bot{self.bot_info['token']}/sendMessage?chat_id={self.bot_info['chatId']}&text={tell}")
+                newMail = driver.find_element(By.XPATH,"//li[contains(@class,'notRead')]//div[@class='mTitle']//strong[@class='mail_title']")
+                ActionChains(driver).click(newMail).perform()
             else:
-                tell = f"신규 팩스 수신, 확인필요\n팩스번호 : {faxNumber}\n원천사 : 확인불가"
-                requests.get(f"https://api.telegram.org/bot{self.bot_info['token']}/sendMessage?chat_id={self.bot_info['chatId']}&text={tell}")
-            newMail = driver.find_element(By.XPATH,"//li[contains(@class,'notRead')]//div[@class='mTitle']//strong[@class='mail_title']")
-            ActionChains(driver).click(newMail).perform()
-        else:
-            pass
+                pass
+        except TimeoutException:
+            driver.quit()
+            t.sleep(5)
+            NFAX.getHome()
     #종료
     def logout(self):
-        logout_profile = driver.find_element(By.XPATH,'//div[@class="profile_area"]')
-        logout_btn = driver.find_element(By.XPATH,'//a[@class="btn logout"]')
-        ActionChains(driver).click(logout_profile).click(logout_btn).perform()
-        t.sleep(1)
+        try:
+            logout_profile = driver.find_element(By.XPATH,'//div[@class="profile_area"]')
+            logout_btn = driver.find_element(By.XPATH,'//a[@class="btn logout"]')
+            ActionChains(driver).click(logout_profile).click(logout_btn).perform()
+            t.sleep(1)
+        except TimeoutException:
+            driver.quit()
+            t.sleep(5)
+            NFAX.getHome()
     def login(self):
-        password_box = driver.find_element(By.XPATH,'//input[@id="user_pwd"]')
-        login_button_2 = driver.find_element(By.XPATH,'//button[@id="loginBtn"]')
-        password = self.works_login['pw']
-        ActionChains(driver).send_keys_to_element(password_box, '{}'.format(password)).click(login_button_2).perform()
-        t.sleep(1)
+        try:
+            password_box = driver.find_element(By.XPATH,'//input[@id="user_pwd"]')
+            login_button_2 = driver.find_element(By.XPATH,'//button[@id="loginBtn"]')
+            password = self.works_login['pw']
+            ActionChains(driver).send_keys_to_element(password_box, '{}'.format(password)).click(login_button_2).perform()
+            t.sleep(1)
+        except TimeoutException:
+            driver.quit()
+            t.sleep(5)
+            NFAX.getHome()
 NFAX = nfax()
 if __name__ == "__main__":
     NFAX.getHome()
