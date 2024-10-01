dc4d0602560990544827be01170ba51a40c11c50
sq1533 <618356@naver.com>
2024-10-02 08:13:08 +0900

    feat: 엔팩스 창으로 변경

--- faxCheck.py
+++ faxCheck.py
@@ -2,93 +2,73 @@ import time as t
 import requests
 import json
 import pandas as pd
-from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException
 from bs4 import BeautifulSoup
-options = webdriver.ChromeOptions()
-options.add_argument("--headless")
-options.add_argument('--disable-gpu')
-options.add_argument("--disable-javascript")
-options.add_argument('--disable-extensions')
-options.add_argument('--blink-settings=imagesEnabled=false')
-driver = webdriver.Chrome(options=options)
 with open('C:\\Users\\USER\\ve_1\\DB\\3loginInfo.json', 'r', encoding='utf-8') as f:
     login_info = json.load(f)
 with open('C:\\Users\\USER\\ve_1\\DB\\6faxInfo.json', 'r',encoding='utf-8') as f:
     fax_info = json.load(f)
-works_login = pd.Series(login_info['worksMail'])
+works_login = pd.Series(login_info['nFax'])
 bot_info = pd.Series(login_info['nFaxbot'])
 fax = pd.DataFrame(fax_info)
 class nfax:
+    def __init__(self):
+        from selenium import webdriver
+        options = webdriver.ChromeOptions()
+        #options.add_argument("--headless")
+        options.add_argument('--disable-gpu')
+        options.add_argument("--disable-javascript")
+        options.add_argument('--disable-extensions')
+        options.add_argument('--blink-settings=imagesEnabled=false')
+        self.driver = webdriver.Chrome(options=options)
     #로그인
     def getHome(self):
-        driver.get("https://mail.worksmobile.com/")
+        self.driver.get("https://www.enfax.com/fax/view/receive")
         t.sleep(1)
         #로그인 정보입력(아이디)
-        id_box = driver.find_element(By.XPATH,'//input[@id="user_id"]')
-        login_button_1 = driver.find_element(By.XPATH,'//button[@id="loginStart"]')
+        id_box = self.driver.find_element(By.XPATH,'//input[@id="userId"]')
         id = works_login['id']
-        ActionChains(driver).send_keys_to_element(id_box, '{}'.format(id)).click(login_button_1).perform()
+        ActionChains(self.driver).send_keys_to_element(id_box, '{}'.format(id)).perform()
         t.sleep(1)
         #로그인 정보입력(비밀번호)
-        password_box = driver.find_element(By.XPATH,'//input[@id="user_pwd"]')
-        login_button_2 = driver.find_element(By.XPATH,'//button[@id="loginBtn"]')
+        password_box = self.driver.find_element(By.XPATH,'//input[@id="userPwd"]')
+        loginbtn = self.driver.find_element(By.XPATH, '//button[@id="btnLogin"]')
         password = works_login['pw']
-        ActionChains(driver).send_keys_to_element(password_box, '{}'.format(password)).click(login_button_2).perform()
+        ActionChains(self.driver).send_keys_to_element(password_box, '{}'.format(password)).click(loginbtn).perform()
         t.sleep(1)
     #엔팩스 메일 확인
-    def newMail(self):
-        try:
-            driver.get("https://mail.worksmobile.com/#/my/103")
-            t.sleep(2)
-            mailHome_soup = BeautifulSoup(driver.page_source,'html.parser')
-            if mailHome_soup.find('li', attrs={'class':'notRead'}) != None:
-                faxNumber = mailHome_soup.find('strong', attrs={'class':'mail_title'}).getText().replace(' ','').split("hecto_2f에")[1].split("로부터")[0]
+    def newFax(self):
+        self.driver.get("https://www.enfax.com/fax/view/receive")
+        t.sleep(2)
+        faxSoup = BeautifulSoup(self.driver.page_source,'html.parser')
+        newfax = faxSoup.find('span', attrs={'class':'state_box stt_notread'})
+        #요소 검증
+        if newfax != None:
+            #수신확인
+            if newfax.get_text()=="안읽음":
+                faxNumber = faxSoup.find('div', attrs={'class':'t_row stt_notread faxReceiveBoxListRow'}).get('data-send-fax-number')
                 if faxNumber in fax['faxNumber'].tolist():
                     tell = f"신규 팩스 수신, 확인필요\n팩스번호 : {faxNumber}\n원천사 : {fax[fax['faxNumber'] == faxNumber]['원천사'].values}"
+                    #텔레그램 전송
                     requests.get(f"https://api.telegram.org/bot{bot_info['token']}/sendMessage?chat_id={bot_info['chatId']}&text={tell}")
+                    t.sleep(2)
                 else:
                     tell = f"신규 팩스 수신, 확인필요\n팩스번호 : {faxNumber}\n원천사 : 확인불가"
+                    #텔레그램 전송
                     requests.get(f"https://api.telegram.org/bot{bot_info['token']}/sendMessage?chat_id={bot_info['chatId']}&text={tell}")
-                newMail = driver.find_element(By.XPATH,"//li[contains(@class,'notRead')]//div[@class='mTitle']//strong[@class='mail_title']")
-                ActionChains(driver).click(newMail).perform()
-            else:
-                pass
-        except TimeoutException:
-            driver.quit()
-            t.sleep(5)
-            NFAX.getHome()
+                    t.sleep(2)
     #종료
     def logout(self):
-        try:
-            logout_profile = driver.find_element(By.XPATH,'//div[@class="profile_area"]')
-            logout_btn = driver.find_element(By.XPATH,'//a[@class="btn logout"]')
-            ActionChains(driver).click(logout_profile).click(logout_btn).perform()
-            t.sleep(1)
-        except TimeoutException:
-            driver.quit()
-            t.sleep(5)
-            NFAX.getHome()
-    def login(self):
-        try:
-            password_box = driver.find_element(By.XPATH,'//input[@id="user_pwd"]')
-            login_button_2 = driver.find_element(By.XPATH,'//button[@id="loginBtn"]')
-            password = works_login['pw']
-            ActionChains(driver).send_keys_to_element(password_box, '{}'.format(password)).click(login_button_2).perform()
-            t.sleep(1)
-        except TimeoutException:
-            driver.quit()
-            t.sleep(5)
-            NFAX.getHome()
+        logout_btn = self.driver.find_element(By.XPATH,'//a[@class="link_logout"]')
+        ActionChains(self.driver).click(logout_btn).perform()
+        t.sleep(1)
 NFAX = nfax()
 if __name__ == "__main__":
-    NFAX.getHome()
     while True:
+        NFAX.getHome()
         for i in range(100):
-            NFAX.newMail()
+            NFAX.newFax()
             t.sleep(5)
         NFAX.logout()
-        NFAX.login()
         t.sleep(0.5)
